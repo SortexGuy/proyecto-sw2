a6fd53ac3f22486e8b7d35662448674ce7373dd2
Rubén A <sortexjuegos@gmail.com>
2025-05-29 22:25:14 -0400

    style: Fix formatting

--- scripts/mono/Loader.cs
+++ scripts/mono/Loader.cs
@@ -1,28 +1,25 @@
 using Godot;
 using System;
 
-public partial class Loader : Node3D
-{
-    public override void _Ready()
-    {
+public partial class Loader : Node3D {
+    public override void _Ready() {
         HttpRequest httpRequest = new HttpRequest();
         this.AddChild(httpRequest);
-        httpRequest.RequestCompleted += (result, responseCode, headers, body) => _HttpRequestCompleted(result, responseCode, headers, body);
+        httpRequest.RequestCompleted += (result, responseCode, headers, body) => {
+            _HttpRequestCompleted(result, responseCode, headers, body);
+        };
         httpRequest.DownloadFile = "./models/pico.glb";
 
         // Perform the HTTP request. The URL below returns a PNG image as of writing.
         Error error = httpRequest.Request("http://localhost:3000/static/pico.glb");
-        if (error != Error.Ok)
-        {
+        if (error != Error.Ok) {
             GD.PushError("An error occurred in the HTTP request: (error code: ).");
         }
     }
 
     // Called when the HTTP request is completed.
-    public void _HttpRequestCompleted(long result, long responseCode, string[] headers, byte[] body)
-    {
-        if (result != (long)HttpRequest.Result.Success)
-        {
+    public void _HttpRequestCompleted(long result, long responseCode, string[] headers, byte[] body) {
+        if (result != (long)HttpRequest.Result.Success) {
             GD.PushError("File couldn't be downloaded.");
         }
         GD.PushWarning(GD.VarToStr(headers));
@@ -34,13 +31,10 @@ public partial class Loader : Node3D
         GltfDocument glDoc = new GltfDocument();
         GltfState glState = new GltfState();
         Error error = glDoc.AppendFromFile("./models/pico.glb", glState);
-        if (error == Error.Ok)
-        {
+        if (error == Error.Ok) {
             Node glRootNode = glDoc.GenerateScene(glState);
             this.AddChild(glRootNode);
-        }
-        else
-        {
+        } else {
             GD.PushError("Couldn't load glTF scene");
         }
     }

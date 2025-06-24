1c5d8d11dbca932b4c15a0e4d59726261bb46a16
Rubén A <sortexjuegos@gmail.com>
2025-06-24 15:12:05 -0400

    feat: Gestos en la vista principal
    Control de la camara (rotacion y zoom)

--- scripts/world_layer.gd
+++ scripts/world_layer.gd
@@ -1,8 +1,27 @@
 extends CanvasLayer
 
 @onready var botones = get_parent().get_node("../Interfaces/WorldOverlayLayer")
+@onready var cam_pivot: Marker3D = %CamPivot
+@onready var cam_pitch: Marker3D = %CamPitch
+@onready var camera_3d: Camera3D = %Camera3D
 
 #Inicializar botones
 func _ready():
 	botones.visible = true
-	
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventGesture or event is InputEventScreenTouch\
+		or event is InputEventScreenDrag:
+		event = event as InputEventGesture
+		if event is InputEventMagnifyGesture:
+			event = event as InputEventMagnifyGesture
+			camera_3d.translate(Vector3.FORWARD * (event.factor-1) * 10)
+			camera_3d.position.z = clampf(camera_3d.position.z, 2, 25)
+		if event is InputEventPanGesture:
+			event = event as InputEventPanGesture
+			cam_pivot.quaternion *= Quaternion(Vector3.UP, event.delta.x * 0.01)
+			cam_pitch.quaternion *= Quaternion(Vector3.RIGHT, event.delta.y * 0.005)
+			cam_pitch.rotation_degrees.x = clampf(cam_pitch.rotation_degrees.x, -85, 15)
+		pass
+	pass

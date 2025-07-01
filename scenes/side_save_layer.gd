662e373f66c6d837b9999839e678e9d6846bd1a3
AltairC11 <altaircr.11@gmail.com>
2025-07-01 15:09:09 -0400

    Delete scenes directory

--- scenes/side_save_layer.gd
+++
@@ -1,7 +0,0 @@
-extends CanvasLayer
-
-
-#Cierra esta ventana y guarda el proyecto
-func _on_button_2_pressed():
-	#Comandos para guardar...
-	self.visible = false
--- scenes/wo_models_layer.gd
+++
@@ -1,7 +0,0 @@
-extends CanvasLayer
-
-
-#Coloca el modelo y cierra la ventana
-func _on_button_pressed():
-	#Se debe colocar la función de agregar el modelo al world...
-	self.visible = false

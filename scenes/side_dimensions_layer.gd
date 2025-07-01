fe1a11a7206b874aebe101981b076338daee4f22
AltairC11 <altaircr.11@gmail.com>
2025-07-01 15:09:09 -0400

    Delete scenes directory

--- scenes/side_dimensions_layer.gd
+++
@@ -1,6 +0,0 @@
-extends CanvasLayer
-
-
-#Cierra esta ventana
-func _on_button_pressed():
-	self.visible = false
--- scenes/world_layer.gd
+++
@@ -1,8 +0,0 @@
-extends CanvasLayer
-
-@onready var botones = get_parent().get_node("../Interfaces/WorldOverlayLayer")
-
-#Inicializar botones
-func _ready():
-	botones.visible = true
-	
